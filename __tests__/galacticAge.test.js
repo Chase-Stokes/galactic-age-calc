ee28d69b0132cb411fa337cde1fd0e7bd7d196dc
Chase-Stokes <chasehstokes@gmail.com>
2021-11-12 12:07:52 -0800

    meaningful fail on test 2

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -3,23 +3,20 @@ import { testPathPatternToRegExp } from 'jest-util';
 import Life from './../src/galacticAge.js';
 
 describe('Life', () => {
-  let user;
-
-  beforeEach(() => {
-    user = new Life(18);
-  });
 
   test('should create an object from the life constructor', () => {
+    let user = new Life(18)
     expect(user.age).toEqual(18);
     expect(user.lifeExpectancy).toEqual(79);
     expect(user.mercury).toEqual(75);
-    expect(user.venus).toEqual(29.03);
-    expect(user.mars).toEqual(9.57);
-    expect(user.jupiter).toEqual(1.52);
+    expect(user.venus).toEqual(29.032258064516128);
+    expect(user.mars).toEqual(9.574468085106384);
+    expect(user.jupiter).toEqual(1.5177065767284992);
   });
 
   test('it should return your life expectancy on mercury', () => {
+    let user = new Life(18)
     user.mercuryExpectancy();
-    expect(output).toEqual(329.16);
+    expect(output).toEqual(329.1666666666667);
   })
 })
